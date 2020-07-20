11c69641558e69087406538e8c11bef1072a1154
Keenan Nemetz <keenan.nemetz@gmail.com>
2020-07-19 18:24:17 -0700

    Update styles for mobile.  Add dark mode support for logo.  Update favicons.

--- logo.js
+++ logo.js
@@ -1,64 +1,72 @@
+const isDarkMode = matchMedia && matchMedia('(prefers-color-scheme: dark)').matches;
+const darkColor = '#fff';
+const lightColor = '#000';
+const spacing = 100;
+
+const defaults = {
+  width: 150,
+  height: 150,
+  fill: true,
+  backface: false,
+  color: isDarkMode ? darkColor : lightColor,
+  stroke: 10,
+  cornerRadius: 5,
+};
+
 let logo = new Zdog.Illustration({
   element: '#logo',
   dragRotate: true,
   rotate: { x: -Zdog.TAU/8, y: -Zdog.TAU/8 },
 });
 
-const spacing = 65;
-
-const defaults = {
-  width: 100,
-  height: 100,
-  fill: true,
-  backface: false,
-  color: '#fff',
-  stroke: 10,
-  cornerRadius: 5,
-};
-
-// top
-new Zdog.RoundedRect({
+let logoTop = new Zdog.RoundedRect({
   ...defaults,
   addTo: logo,
   translate: { y: -spacing },
   rotate: { x: Zdog.TAU/4 },
 });
 
-// front
-new Zdog.RoundedRect({
+let logoFront = new Zdog.RoundedRect({
   ...defaults,
   addTo: logo,
   translate: { z: spacing },
 });
 
-// back
-new Zdog.RoundedRect({
+let logoBack = new Zdog.RoundedRect({
   ...defaults,
   addTo: logo,
   translate: { z: -spacing },
   rotate: { y: Zdog.TAU/2 },
 });
 
-// left
-new Zdog.RoundedRect({
+let logoLeft = new Zdog.RoundedRect({
   ...defaults,
   addTo: logo,
   translate: { x: -spacing },
   rotate: { y: Zdog.TAU/4 },
 });
 
-// right
-new Zdog.RoundedRect({
+let logoRight = new Zdog.RoundedRect({
   ...defaults,
   addTo: logo,
   translate: { x: spacing },
   rotate: { y: -Zdog.TAU/4 },
 });
 
+function onDarkModeChanged(event) {
+  logoTop.color = event.matches ? darkColor : lightColor;
+  logoFront.color = event.matches ? darkColor : lightColor;
+  logoBack.color = event.matches ? darkColor : lightColor;
+  logoLeft.color = event.matches ? darkColor : lightColor;
+  logoRight.color = event.matches ? darkColor : lightColor;
+}
+
 function animate() {
   logo.rotate.x = -Zdog.TAU/8;
   logo.updateRenderGraph();
   requestAnimationFrame(animate);
 }
+
+if (matchMedia) matchMedia('(prefers-color-scheme: dark)').addListener(onDarkModeChanged);
 
 animate();
