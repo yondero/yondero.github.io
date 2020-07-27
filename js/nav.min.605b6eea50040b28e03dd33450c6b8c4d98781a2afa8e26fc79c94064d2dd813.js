ced85dabd4a7a37ad7eb5eb1551d59058c226197
Keenan Nemetz <keenan.nemetz@gmail.com>
2020-07-27 15:19:04 -0700

    rebuilding site Mon Jul 27 15:19:04 PDT 2020

---
+++ js/nav.min.605b6eea50040b28e03dd33450c6b8c4d98781a2afa8e26fc79c94064d2dd813.js
@@ -0,0 +1,1 @@
+let button=document.getElementById('navbar-button');let menu=document.getElementById('navbar-menu');button.addEventListener('click',()=>{button.classList.toggle('is-active');menu.classList.toggle('is-active');});
